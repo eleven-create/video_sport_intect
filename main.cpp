40dd18742588561fee9b48d1a83d6cc42e8771a2
guo <iris91414@gmail.com>
2026-06-15 17:44:58 +0800

    feat: 完成摄像头读取与灰度预处理代码。

--- main.cpp
+++ main.cpp
@@ -1,15 +1,43 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
-
+using namespace cv;
+using namespace std;
 int main()
 {
-    cv::Mat img = cv::Mat::zeros(500, 800, CV_8UC3);
-    cv::circle(img, cv::Point(400, 250), 120, cv::Scalar(0, 255, 0), 3);
+    system("chcp 65001"); 
 
-    cv::imshow("OpenCV Test", img);
-    std::cout << "OpenCV 版本: " << CV_VERSION << std::endl;
+    VideoCapture cap(0); 
+    
+    if (!cap.isOpened()) {
+        cout << "报告：摄像头打不开！请检查设备连接。" << endl;
+        return -1;
+    }
 
-    cv::waitKey(0);
-    cv::destroyAllWindows();
+    Mat frame;      
+    Mat grayFrame;  
+
+    while (true) {
+        cap >> frame; 
+        
+        if (frame.empty()) {
+            cout << "读取不到画面，视频播放结束！" << endl;
+            break; 
+        }
+
+        // 核心动作：转换为黑白图
+        cvtColor(frame, grayFrame, COLOR_BGR2GRAY);
+
+        // 放映室
+        imshow("彩色原图", frame);
+        imshow("黑白图", grayFrame);
+
+        if (waitKey(30) == 27) {
+            cout << "接收到 ESC 指令，程序退出。" << endl;
+            break;
+        }
+    }
+    
+    cap.release();
+    destroyAllWindows();
     return 0;
 }
