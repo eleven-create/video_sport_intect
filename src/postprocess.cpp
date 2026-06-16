6c26fc543ead9779226f2464e69df7d2224ac7ff
guo <iris91414@gmail.com>
2026-06-16 09:19:32 +0800

    更改postprocess.h和postprocess.cpp

---
+++ src/postprocess.cpp
@@ -0,0 +1,11 @@
+#include "postprocess.h"
+#include <iostream>
+void testHSVShadow(cv::Mat &frame) {
+    cv::Mat hsvFrame,mask;
+    cv::cvtColor(frame,hsvFrame,cv::COLOR_BGR2HSV);//将色彩原图转化为hsv格式
+    cv::Scalar lowBound(0,0,0);
+    cv::Scalar upperBound(180,255,80);//设定色彩的hsv上下限
+    cv::inRange(hsvFrame,lowBound,upperBound,mask);
+    cv::imshow("HSV处理结果",hsvFrame);
+    cv::imshow("抠图结果（白色为符合条件的）",mask);
+}
